06cebca285179296c2acf7985655e259bd5c5e17
“umashivalingaiah2” <uma.shivalingaiah2@sky.uk>
2022-01-05 12:10:16 +0000

    removed unnecessary steps

---
+++ src/main/java/com/equalexperts/shoppingcart/ShoppingCart.java
@@ -0,0 +1,54 @@
+package com.equalexperts.shoppingcart;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+
+    private final List<Product> shoppingCartProducts = new ArrayList<>();
+    private BigDecimal totalPriceWithSalesTax = BigDecimal.ZERO;
+    private BigDecimal totalSalesTax = BigDecimal.ZERO;
+    private static final BigDecimal APPLICABLE_TAX_RATE = new BigDecimal("0.125"); //0.125 is equal to 12.5%
+
+    public List<Product> getShoppingCartProducts() {
+        return shoppingCartProducts;
+    }
+
+    public void addToShoppingCart(String productName, BigDecimal unitPrice, int quantity) {
+        List<Product> products = new ArrayList<>();
+        if (quantity > 0) {
+            products.add(buildProduct(productName, unitPrice, quantity));
+        }
+        shoppingCartProducts.addAll(products);
+    }
+
+    public String getShoppingCartTotalSalesTax() {
+        return applyRoundRule(getTotalSalesTax());
+    }
+
+    public String getShoppingCartTotalPrice() {
+        BigDecimal totalPrice = BigDecimal.ZERO;
+        for(Product product: shoppingCartProducts) {
+            totalPrice = totalPrice.add(BigDecimal.valueOf(product.getQuantity()).multiply(product.getUnitPrice()));
+            this.totalPriceWithSalesTax = totalPrice.add(totalPrice.multiply(APPLICABLE_TAX_RATE));
+            this.totalSalesTax = this.totalSalesTax.add(BigDecimal.valueOf(product.getQuantity()).multiply(product.getUnitPrice()).multiply(APPLICABLE_TAX_RATE));
+        }
+        return applyRoundRule(totalPriceWithSalesTax);
+    }
+
+    private BigDecimal getTotalSalesTax() {
+        return this.totalSalesTax;
+    }
+
+    private Product buildProduct(String productName, BigDecimal unitPrice, int quantity) {
+        return Product.builder().productName(productName).unitPrice(unitPrice).quantity(quantity).build();
+    }
+
+    private String applyRoundRule(BigDecimal input) {
+        BigDecimal result = input.setScale(2, RoundingMode.HALF_UP);
+        return result.toString();
+    }
+
+}
---
+++ src/test/java/com/equalexperts/shoppingcart/ShoppingCartTest.java
@@ -0,0 +1,80 @@
+package com.equalexperts.shoppingcart;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ShoppingCartTest {
+
+    private ShoppingCart shoppingCart = new ShoppingCart();
+
+    @Test
+    public void givenAnEmptyShoppingCart_whenProductsAddedWithZeroQuantity_thenCheckTotalPriceAndSalesTax() {
+        //Given
+        assertThat(shoppingCart.getShoppingCartProducts().size()).isEqualTo(0);
+
+        //When
+        shoppingCart.addToShoppingCart("Dove Soap", new BigDecimal("39.99"), 0);
+
+        // Then
+        assertThat(shoppingCart.getShoppingCartProducts().size()).isEqualTo(0);
+        assertThat(shoppingCart.getShoppingCartTotalPrice()).isEqualTo("0.00");
+        assertThat(shoppingCart.getShoppingCartTotalSalesTax()).isEqualTo("0.00");
+    }
+
+    @Test
+    public void givenAnEmptyShoppingCart_whenProductsAddedWithNegativeQuantity_thenCheckTotalPriceAndSalesTax() {
+        //Given
+        assertThat(shoppingCart.getShoppingCartProducts().size()).isEqualTo(0);
+
+        //When
+        shoppingCart.addToShoppingCart("Dove Soap", new BigDecimal("39.99"), -1);
+
+        // Then
+        assertThat(shoppingCart.getShoppingCartProducts().size()).isEqualTo(0);
+        assertThat(shoppingCart.getShoppingCartTotalPrice()).isEqualTo("0.00");
+        assertThat(shoppingCart.getShoppingCartTotalSalesTax()).isEqualTo("0.00");
+    }
+
+    @Test
+    public void givenAnEmptyShoppingCart_whenFiveSoapsAdded_thenCheckTotalPriceAndTotalSalesTax() {
+        //Given
+        assertThat(shoppingCart.getShoppingCartProducts().size()).isEqualTo(0);
+
+        // When
+        shoppingCart.addToShoppingCart("Dove Soap", new BigDecimal("39.99"), 5);
+
+        // Then
+        Product dove_soap = Product.builder().productName("Dove Soap").unitPrice(new BigDecimal("39.99")).quantity(5).build();
+        assertThat(shoppingCart.getShoppingCartProducts()).containsAll(List.of(dove_soap));
+
+        assertThat(shoppingCart.getShoppingCartTotalPrice()).isEqualTo("224.94");
+        assertThat(shoppingCart.getShoppingCartTotalSalesTax()).isEqualTo("24.99");
+    }
+
+    @Test
+    public void givenAnEmptyShoppingCart_whenMultipleProductsAdded_thenCheckTotalPriceAndTotalSalesTax() {
+        //Given
+        assertThat(shoppingCart.getShoppingCartProducts().size()).isEqualTo(0);
+        BigDecimal doveSoapPrice  = new BigDecimal("39.99");
+        BigDecimal axeDeoPrice = new BigDecimal("99.99");
+
+        // When
+        shoppingCart.addToShoppingCart("Dove Soap", doveSoapPrice, 2);
+        shoppingCart.addToShoppingCart("Axe Deos",  axeDeoPrice, 2);
+
+        //Then
+        Product dove_soap = Product.builder().productName("Dove Soap").unitPrice(doveSoapPrice).quantity(2).build();
+        Product axe_deos = Product.builder().productName("Axe Deos").unitPrice(axeDeoPrice).quantity(2).build();
+        assertThat(shoppingCart.getShoppingCartProducts()).containsAll(Arrays.asList(dove_soap, axe_deos));
+
+        assertThat(shoppingCart.getShoppingCartTotalPrice()).isEqualTo("314.96");
+        assertThat(shoppingCart.getShoppingCartTotalSalesTax()).isEqualTo("35.00");
+    }
+
+
+}
